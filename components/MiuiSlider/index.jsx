6de538a60fa840007816a055e64a24d908363678
Naman Agarwal <namanddev7@gmail.com>
2022-01-03 10:46:06 +0530

    add Miui Slider

--- components/MiuiSlider/index.jsx
+++ components/MiuiSlider/index.jsx
@@ -1,40 +1,51 @@
 import React, { useRef } from "react";
-import {
-  View,
-  PanResponder
-} from "react-native";
+import { View, PanResponder } from "react-native";
 
 import Animated, {
-    useSharedValue,
-    useAnimatedStyle
-  } from "react-native-reanimated";
-  
+  useSharedValue,
+  useAnimatedStyle,
+} from "react-native-reanimated";
+
 export default function MiuiSlider() {
-    
-    const posX = useSharedValue(10);
+  const posX = useSharedValue(10);
 
-    const updatePosX = (dx) => {
-        let temp = posX.value + dx * 0.01
-        temp = temp < 0? 0: temp > 100 ? 100: temp
-        posX.value = temp
-    }
+  const updatePosX = (dx) => {
+    let temp = posX.value + dx * 0.02;
+    temp = temp < 0 ? 0 : temp > 100 ? 100 : temp;
+    posX.value = temp;
+  };
 
-    const panResponder = useRef(
-        PanResponder.create({
-            onMoveShouldSetPanResponder: () => true,
-            onPanResponderMove: (evt, g) => updatePosX(g.dx)
-        })
-    ).current
+  const panResponder = useRef(
+    PanResponder.create({
+      onMoveShouldSetPanResponder: () => true,
+      onPanResponderMove: (evt, g) => updatePosX(g.dx),
+    })
+  ).current;
 
-    const animatedWidth = useAnimatedStyle(() => {
-        return {
-          width: posX.value + '%',
-        }
-    });
+  const animatedWidth = useAnimatedStyle(() => {
+    return {
+      width: posX.value + "%",
+    };
+  });
 
-    return (
-      <View style={{ backgroundColor: "lightgrey", justifyContent: "center", height: 50, borderRadius: 10, margin: 20 }} {...panResponder.panHandlers}>
-          <Animated.View style={[{ height: "100%", backgroundColor: "red", borderRadius: 10 }, animatedWidth]}></Animated.View>
-      </View>
-    );
-  }
+  return (
+    <View
+      style={{
+        backgroundColor: "lightgrey",
+        justifyContent: "center",
+        height: 50,
+        borderRadius: 10,
+        margin: 20,
+        width: "100%",
+      }}
+      {...panResponder.panHandlers}
+    >
+      <Animated.View
+        style={[
+          { height: "100%", backgroundColor: "red", borderRadius: 10 },
+          animatedWidth,
+        ]}
+      ></Animated.View>
+    </View>
+  );
+}
